947a1311e6bc17efd3646afc97bc0605609ba9c9
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-04-07 14:26:27 +1000

    Initial layout

---
+++ src/Control/Quiver/Instances.hs
@@ -0,0 +1,13 @@
+{- |
+   Module      : Control.Quiver.Instances
+   Description : Extra instances for Quiver
+   Copyright   : (c) Ivan Lazar Miljenovic
+   License     : MIT
+   Maintainer  : Ivan.Miljenovic@gmail.com
+
+
+
+ -}
+module Control.Quiver.Instances () where
+
+import Control.Quiver
