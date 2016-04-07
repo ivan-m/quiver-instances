a5bde4009f37ba4b6c166973f2ac6399f8545b55
Ivan Lazar Miljenovic <Ivan.Miljenovic@gmail.com>
2016-04-07 17:00:48 +1000

    MonadThrow and MonadCatch instances

--- src/Control/Quiver/Instances.hs
+++ src/Control/Quiver/Instances.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
 {- |
    Module      : Control.Quiver.Instances
    Description : Extra instances for Quiver
@@ -10,4 +12,23 @@
  -}
 module Control.Quiver.Instances () where
 
-import Control.Quiver
+import Control.Quiver.Internal
+import Control.Quiver.SP
+
+import Control.Monad       (join)
+import Control.Monad.Catch
+
+--------------------------------------------------------------------------------
+
+-- | Throws exceptions into the base monad.
+instance (MonadThrow f) => MonadThrow (P a a' b b' f) where
+  throwM = qlift . throwM
+
+instance (MonadCatch f) => MonadCatch (P a a' b b' f) where
+  catch p onErr = go p
+    where
+      go p' = case p' of
+                Consume x k q -> consume x (go . k) (decouple (go q))
+                Produce y k q -> produce y (go . k) (deplete  (go q))
+                Enclose m -> enclose (catch (go <$> m) (return . onErr))
+                Deliver r -> deliver r
